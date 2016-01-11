7293668674b21ad06a1b770cc7c31b5335674e08
matt <palcente@gmail.com>
2016-01-11 20:47:37 +0000

    test environment

---
+++ src/main/resources/sql/utils.sql
@@ -0,0 +1,4 @@
+update batch_step_execution set 
+start_time = timestampadd('DAY',(select timestampdiff('DAY',max(start_time),current_timestamp()) from batch_step_execution),start_time),
+end_time = timestampadd('DAY',(select timestampdiff('DAY',max(start_time),current_timestamp()) from batch_step_execution),last_updated),
+last_updated= timestampadd('DAY',(select timestampdiff('DAY',max(start_time),current_timestamp()) from batch_step_execution),last_updated);
